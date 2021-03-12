27885117eb15dd443adfa34a33598f0d98d09c64
Robson Lawson <robsonlawson3@gmail.com>
2021-03-12 19:57:26 +0000

    Add utility function to make pushing / poping child configs easier
    Signed-off-by: Robson Lawson <robsonlawson3@gmail.com>

--- src/main/java/xyz/spgamers/forge/armageddon/config/ClientConfig.java
+++ src/main/java/xyz/spgamers/forge/armageddon/config/ClientConfig.java
@@ -2,14 +2,25 @@ package xyz.spgamers.forge.armageddon.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.function.Function;
+
 public final class ClientConfig
 {
 	public final ForgeConfigSpec configSpec;
+	private final ForgeConfigSpec.Builder configBuilder;
 
 	public ClientConfig()
 	{
-		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+		configBuilder = new ForgeConfigSpec.Builder();
 
-		configSpec = builder.build();
+		configSpec = configBuilder.build();
+	}
+
+	private <T> T config(String configName, Function<ForgeConfigSpec.Builder, T> configBuilder)
+	{
+		this.configBuilder.push(configName);
+		T config = configBuilder.apply(this.configBuilder);
+		this.configBuilder.pop();
+		return config;
 	}
 }
--- src/main/java/xyz/spgamers/forge/armageddon/config/ServerConfig.java
+++ src/main/java/xyz/spgamers/forge/armageddon/config/ServerConfig.java
@@ -2,14 +2,26 @@ package xyz.spgamers.forge.armageddon.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.function.Function;
+
 public final class ServerConfig
 {
 	public final ForgeConfigSpec configSpec;
 
+	private final ForgeConfigSpec.Builder configBuilder;
+
 	public ServerConfig()
 	{
-		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+		configBuilder = new ForgeConfigSpec.Builder();
 
-		configSpec = builder.build();
+		configSpec = configBuilder.build();
+	}
+
+	private <T> T config(String configName, Function<ForgeConfigSpec.Builder, T> configBuilder)
+	{
+		this.configBuilder.push(configName);
+		T config = configBuilder.apply(this.configBuilder);
+		this.configBuilder.pop();
+		return config;
 	}
 }
