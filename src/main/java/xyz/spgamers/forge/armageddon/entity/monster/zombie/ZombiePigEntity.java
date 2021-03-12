74361421a8307d454765735ec37e1da804add9ee
Robson Lawson <robsonlawson3@gmail.com>
2021-03-12 23:05:41 +0000

    Make ZombiePig use Pig sounds rather than Zombie sounds
    Signed-off-by: Robson Lawson <robsonlawson3@gmail.com>

--- src/main/java/xyz/spgamers/forge/armageddon/entity/monster/zombie/ZombiePigEntity.java
+++ src/main/java/xyz/spgamers/forge/armageddon/entity/monster/zombie/ZombiePigEntity.java
@@ -5,6 +5,9 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
@@ -18,6 +21,36 @@ public final class ZombiePigEntity extends AbstractZombieEntity
 	public ZombiePigEntity(World world)
 	{
 		super(ModEntities.ZOMBIE_PIG.get(), world, Armageddon.SERVER_CONFIG.entities::isZombiePigEnabled);
+	}
+
+	@Override
+	protected void registerGoals()
+	{
+		super.registerGoals();
+	}
+
+	@Override
+	protected SoundEvent getAmbientSound()
+	{
+		return SoundEvents.ENTITY_PIG_AMBIENT;
+	}
+
+	@Override
+	protected SoundEvent getHurtSound(DamageSource damageSource)
+	{
+		return SoundEvents.ENTITY_PIG_HURT;
+	}
+
+	@Override
+	protected SoundEvent getDeathSound()
+	{
+		return SoundEvents.ENTITY_PIG_DEATH;
+	}
+
+	@Override
+	protected SoundEvent getStepSound()
+	{
+		return SoundEvents.ENTITY_PIG_STEP;
 	}
 
 	public static AttributeModifierMap.MutableAttribute registerZombiePigAttributes()
